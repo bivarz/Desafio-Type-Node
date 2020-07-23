6e2b84763844a96c6678af85350e27b8b018cc67
bivarz <paulo.bivar@gmail.com>
2020-07-23 00:07:25 -0300

    Ending

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,9 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!["income", "outcome"].includes(type)) {
+      throw new Error(`The type: ${type}, is not valid, try: income or outcome `)
+    }
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
